db96b777f62fc44312f4f0581d54682dc703648c
Edmore Moyo <edmore@linux-m7qo.site>
2012-07-16 20:33:31 +0200

    Takes a list of lists and returns a list of the first atoms

---
+++ firsts.scm
@@ -0,0 +1,7 @@
+; Takes a list of lists and returns a list of the first atoms from the individual lists
+(define firsts
+  (lambda(ll)
+    (cond
+    ((null? ll) '())
+    (else (cons (car(car ll)) (firsts (cdr ll))))
+    )))
