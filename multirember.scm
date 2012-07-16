27dca9c1675756f4045b002cc51acd10b4473d5e
Edmore Moyo <edmore@linux-m7qo.site>
2012-07-16 20:26:58 +0200

    Multirember - remove all occurences of an atom within a list

---
+++ multirember.scm
@@ -0,0 +1,8 @@
+; Remove all occurences of an atom
+(define multirember
+  (lambda(a lat)
+    (cond
+      ((null? lat) '()) 
+      ((eq? (car lat) a) (multirember a (cdr lat)))
+      (else (cons (car lat) (multirember a (cdr lat ))))
+      )))
