1b809db56c33eecf1c4e4cf5e47851348922ea59
Edmore Moyo <etmoyo@Moyo-Mac.(none)>
2012-09-06 20:03:52 +0200

    Update tup_plus to support tuples of different lengths

--- tup_plus.scm
+++ tup_plus.scm
@@ -1,6 +1,8 @@
 ; Add consecutive numeric tuples to one another
 (define tup+
- (lambda(x y)
-  (cond
-   ((and (null? x)(null? y)) '())
-   (else (cons (+ (car x)(car y)) (tup+ (cdr x)(cdr y)))))))
+  (lambda(tup1 tup2)
+    (cond
+     ((null? tup1) tup2)
+     ((null? tup2) tup1)
+     ((and (null? tup1)(null? tup2)) '())
+     (else (cons (+ (car tup1)(car tup2)) (tup+ (cdr tup1)(cdr tup2)))))))
